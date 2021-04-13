7361687dbc49ee20fda5d301db675d58fb5d1170
Urian <urian1213viera@gmail.com>
2021-04-13 16:48:35 -0500

    quitando una ruta

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'NoticiasController@noticiasHome')->name('noticiasHome');
-
-
-
-Route::get('/eje', 'NoticiasController@ejemplo')->name('ejemplo');
 Route::get('StatusNoticia', 'NoticiasController@UpdateStatusNoti')->name('UpdateStatusNoti');
